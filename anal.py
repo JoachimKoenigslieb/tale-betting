3643d5ce2e7f29d2c225919f80cb92043147eefa
Joachim Kønigslieb <joachim.koenigslieb@gmail.com>
2022-12-31 15:22:12 +0100

    det kan godt blive dummere

--- anal.py
+++ anal.py
@@ -13,78 +13,131 @@ import numpy as np
 import math
 
 def poisson(k, lamb):
-    return math.exp(-lamb) * lamb**k / math.factorial(k)
+	return math.exp(-lamb) * lamb**k / math.factorial(k)
 
 def CDF_poisson(k, lamb):
-    return sum([poisson(i, lamb) for i in range(k+1)])
+	return sum([poisson(i, lamb) for i in range(k+1)])
 
 def cumsum(y):
-    s = [y[0]]
-    for i in range(1, len(y)):
-        s.append(s[-1] + y[i])
-    return np.array(s)
+	s = [y[0]]
+	for i in range(1, len(y)):
+		s.append(s[-1] + y[i])
+	return np.array(s)
 
 with open('texts', 'rb') as file:
-    texts = pickle.load(file)
-    
-all_words = {}
+	texts = pickle.load(file)
+	
+odds_info = [
+	{
+		'word': 'danmark',
+		'over/under': (1.95, 1.75, ),
+		'threshold': 7.5,
+	},
+	{
+		'word': 'danske',
+		'over/under': (1.80, 1.90, ),
+		'threshold': 4.5,
+	},
+	{
+		'word': 'tak',
+		'over/under': (1.70, 2.02, ),
+		'threshold': 3.5,
+	},
+	{
+		'word': 'grønland',
+		'over/under': (1.62, 2.15, ),
+		'threshold': 2.5,
+	},
+	{
+		'word': 'færøerne',
+		'over/under': (2.2, 1.6, ),
+		'threshold': 2.5,
+	},
+	{
+		'word': 'nytår',
+		'over/under': (1.95, 1.75, ),
+		'threshold': 2.5,
+	},
+	{
+		'word': 'samfund',
+		'over/under': (1.85, 1.85, ),
+		'threshold': 2.5,
+	},
+	{
+		'word': 'verden',
+		'over/under': (1.60, 2.2, ),
+		'threshold': 2.5,
+	},
+	{
+		'word': 'nytårsønsker',
+		'over/under': (2.2, 1.6, ),
+		'threshold': 2.5,
+	},
+	{
+		'word': 'varmeste',
+		'over/under': (1.48, 2.45, ),
+		'threshold': 1.5,
+	},
+]
 
-over_under_words = ['danmark', 'danske', 'verden', 'tak', 'nytår', 'familie', 'samfund', 'grønland', 'færøerne']
+over_under_words = [odds['word'] for odds in odds_info]
+over_under_line = [odds['threshold'] for odds in odds_info]
+over_under_odds = [odds['over/under'] for odds in odds_info]
+
 over_under_count = {word: [] for word in over_under_words}
-over_under_line = [6.5, 4.5, 4.5, 4.5,  3.5, 3.5, 3.5, 2.5, 2.5]
-over_under_odds = [(1.6, 2.2), (1.72, 2), (1.6, 2.2), (1.85, 1.85), (1.75, 1.95), (1.65, 2.1), (2.2, 1.6), (1.85, 1.85), (2.3, 1.55)]
-over_under_assigned_prob = []
+asigned_over_probs = []
 kellys = []
 
 punctuation = [',', '.', ':', '!', ';']
 
-for text in texts:
-    words = text.split(' ')
-    
-    for word in words:
-        for symbol in punctuation:
-            word = word.replace(symbol, '')
-        if word in all_words:
-            all_words[word] += 1
-        else:
-            all_words[word] = 1
-    for word in over_under_words:
-        over_under_count[word].append(words.count(word))
+for text_ind, text in enumerate(texts):
+	year = 1972 + text_ind 
+
+	print('parsing year...', year)
+
+	words = text.split(' ')
+	
+	for word in over_under_words:
+		over_under_count[word].append(words.count(word))
 
 for word in over_under_words: 
-    over_under_count[word] = np.array(over_under_count[word])
+	over_under_count[word] = np.array(over_under_count[word])
 
-f, a = plt.subplots(3,3, figsize=(10, 7))
-f1, a1 = plt.subplots(3, 3, figsize=(10, 7))
+plot_x = 5
+plot_y = 2
 
-for i in range(3):
-    for j in range(3):
-        word = over_under_words[i*3 + j]
-        count = np.array(over_under_count[word])[:]
+f, a = plt.subplots(plot_x, plot_y, figsize=(10, 7))
+f1, a1 = plt.subplots(plot_x, plot_y, figsize=(10, 7))
 
-        lamb = count.mean() #max-likelyhood estimator for lambda in poission distriubution is just the mean!
-        p = 1 - CDF_poisson(int(over_under_line[i*3 + j]), lamb) #tail of distribution
-        over_under_assigned_prob.append(p)
+for i in range(plot_y):
+	for j in range(plot_x):
+		word = over_under_words[i*plot_x + j]
+		count = np.array(over_under_count[word])[:]
 
-        under_odds = over_under_odds[i*3 + j][0] 
-        b = under_odds - 1
-        
-        kelly = (b * (1-p) + (1-p) - 1)/b
-        kellys.append(kelly)
-        
-        a[i, j].plot(count, 'o')
-        yticks = range(0, count.max()+1) if count.max() < 6 else range(0, count.max()+2, 2)
-        a[i, j].set_yticks(yticks)
-        a[i, j].plot([0, len(count)], [over_under_line[i*3 + j]] * 2, 'red')
-        a[i, j].set_title(f'{word}. assigned proba: {100 * p:0.3f}%')
+		lamb = count.mean() #max-likelyhood estimator for lambda in poission distriubution is just the mean!
+		p = 1 - CDF_poisson(int(over_under_line[i*plot_x + j]), lamb) #tail of distribution
+		asigned_over_probs.append(p)
 
-        labels, counts = np.unique(count, return_counts=True)
-        poisson_fit = [poisson(i, lamb) for i in range(len(labels) + 1)]
+		under_odds = over_under_odds[i*plot_x + j][0] 
+		b = under_odds - 1
+		
+		kelly = (b * (1-p) + (1-p) - 1)/b
+		kellys.append(kelly)
+		
+		a[j, i].plot(count, 'o')
+		yticks = range(0, count.max()+1) if count.max() < 6 else range(0, count.max()+2, 2)
+		a[j, i].set_yticks(yticks)
+		a[j, i].plot([0, len(count)], [over_under_line[i*plot_x + j]] * 2, 'red')
+		a[j, i].set_title(f'"{word}"')
+		a[j, i].set_xlabel('Years (after 1972)')
 
-        a1[i, j].set_title(f'{word}')
-        a1[i, j].bar(labels, counts/counts.sum(), align='center')
-        a1[i, j].set_xticks(labels)
-        a1[i, j].plot(range(len(labels)+1), poisson_fit, 'r--')
+		labels, counts = np.unique(count, return_counts=True)
+		poisson_fit = [poisson(i, lamb) for i in range(len(labels) + 1)]
+
+		a1[j, i].set_title(f'{word}')
+		a1[j, i].bar(labels, counts/counts.sum(), align='center')
+		a1[j, i].set_xticks(labels)
+		a1[j, i].plot(range(len(labels)+1), poisson_fit, 'r--')
 
 money = 1000
 kellys = np.array(kellys)
@@ -93,11 +146,14 @@ distribute_odds = kellys/kellys.sum() * money
 
 payout = distribute_odds * [odds[0] for odds in over_under_odds] 
 
-for i in range(3):
-    for j in range(3):
-        word = over_under_words[i*3 + j]
-        suggested_bet = distribute_odds[i*3 + j]
-        a1[i, j].set_title(f'{word}. suggested bet:  {suggested_bet/money*100:0.3f}%')
+for i in range(plot_y):
+	for j in range(plot_x):
+		word = over_under_words[i*plot_x + j]
+		suggested_bet = distribute_odds[i*plot_x + j]
+		over_prop = asigned_over_probs[i * plot_x + j] * 100
+		under_props = 100 - over_prop
+
+		a1[j, i].set_title(f'"{word}"\nAssigned over/under : {over_prop:0.1f}%/{under_props:0.1f}%. Kelly: {kellys[i*plot_x + j]:0.2f}')
    
 f.tight_layout()
 f1.tight_layout()
@@ -106,4 +162,4 @@ f.savefig('fig1.png')
 f1.savefig('fig2.png')
 
 for odd, word in zip(distribute_odds, over_under_words):
-    print(word, int(odd))
+	print(word, int(odd))
--- scrape.py
+++ scrape.py
@@ -12,33 +12,40 @@ from bs4 import BeautifulSoup as bs
 import time
 import pickle 
 
-# texts = []
-# for year in range(1972, 2020):
-#     if year == 1976:
-#         url = f'https://dansketaler.dk/tale/h-m-dronning-margrethe-iis-nytaarstale-1976/print/'
-#     elif year == 2012:
-#         url = f'https://dansketaler.dk/tale/dronningens-nytarstale-2012/print'
-#     elif year == 2016:
-#         url = 'https://dansketaler.dk/tale/dronningens-nytaartale-2016/print'
-#     elif year == 2017:
-#         url = 'https://dansketaler.dk/tale/h-m-dronningens-nytaarstale-2017/print'
-#     elif year == 2018:
-#         url = 'https://dansketaler.dk/tale/hendes-majestaet-dronningens-nytaarstale-2018/print'
-#     elif year == 2019:
-#         url = 'https://dansketaler.dk/tale/h-m-dronning-margrethe-iis-nytaarstale-2019/print'
-#     elif year == 1979:
-#         url = f'https://dansketaler.dk/tale/christian-9-palae-amalienborg/print'
-#     else:
-#         url = f'https://dansketaler.dk/tale/dronningens-nytaarstale-{year}/print/'
+texts = []
+for year in range(1972, 2022):
+	if year == 1976:
+		url = f'https://dansketaler.dk/tale/h-m-dronning-margrethe-iis-nytaarstale-1976/print/'
+	elif year == 2012:
+		url = f'https://dansketaler.dk/tale/dronningens-nytarstale-2012/print'
+	elif year == 2016:
+		url = 'https://dansketaler.dk/tale/dronningens-nytaartale-2016/print'
+	elif year == 2017:
+		url = 'https://dansketaler.dk/tale/h-m-dronningens-nytaarstale-2017/print'
+	elif year == 2018:
+		url = 'https://dansketaler.dk/tale/hendes-majestaet-dronningens-nytaarstale-2018/print'
+	elif year == 2019:
+		url = 'https://dansketaler.dk/tale/h-m-dronning-margrethe-iis-nytaarstale-2019/print'
+	elif year == 1979:
+		url = f'https://dansketaler.dk/tale/christian-9-palae-amalienborg/print'
+	elif year == 2020:
+		url = 'https://dansketaler.dk/tale/h-m-dronning-margrethe-iis-nytaarstale-2020/print'
+	elif year == 2021:
+		url = 'https://dansketaler.dk/tale/h-m-dronning-margrethe-iis-nytaarstale-2021-2/print'
+	else:
+		url = f'https://dansketaler.dk/tale/dronningens-nytaarstale-{year}/print/'
 
-#     print(f'downlading {year}...')
-#     r = requests.get(url)
-#     time.sleep(0.5)
-#     soup = bs(r.text)
-#     text = soup.find('div', class_='entry-content').text
-#     texts.append(text)
+	print(f'downlading {year}...')
+	r = requests.get(url)
+	# time.sleep(0.5)
+	soup = bs(r.text)
+	text = soup.find('div', class_='entry-content').text
+	texts.append(text)
+
+	with open(f'./taler/{year}.txt', 'w') as file:
+		file.write(text)
 
 texts = [text.replace('\n', ' ').strip().lower() for text in texts]
 
 with open('texts', 'wb') as file:
-    pickle.dump(texts, file)
+	pickle.dump(texts, file)
